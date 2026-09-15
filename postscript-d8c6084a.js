90d7455a48ce05490c5bc5d76533538dda41bef0
Tomas Madeja <tomas.madeja@protonmail.com>
2026-09-15 17:07:07 +0200

    updating page

--- postscript-d8c6084a.js
+++ postscript-d8c6084a.js
@@ -2,8 +2,8 @@ await Promise.all([
   import("./static/scripts/script-0-f7c52b6a.js"),
   import("./static/scripts/script-1-0e41d61b.js"),
   import("./static/scripts/script-2-f1365b3a.js"),
-  import("./static/scripts/script-3-1f9394e6.js"),
-  import("./static/scripts/script-4-af2773e5.js"),
+  import("./static/scripts/script-3-4e3473f6.js"),
+  import("./static/scripts/script-4-8be19c3a.js"),
   import("./static/scripts/script-5-cc8afa9d.js"),
   import("./static/scripts/script-6-7e107d7e.js"),
   import("./static/scripts/script-7-5ef560a8.js"),
@@ -13,4 +13,4 @@ await Promise.all([
   import("./static/scripts/script-11-be15df42.js"),
   import("./static/scripts/script-12-a2f22c29.js")
 ]);
-await import("./static/scripts/script-13-d93af9ba.js");
+await import("./static/scripts/script-13-4a1444dc.js");
